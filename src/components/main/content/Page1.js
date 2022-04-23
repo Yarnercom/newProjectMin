13a9a84e20a31d498e619fa8dcdd8fb3126437a8
ACER <a.doroev@mail.ru>
2022-04-23 11:11:54 +0600

    saveCommit2

--- src/components/main/content/Page1.js
+++ src/components/main/content/Page1.js
@@ -12,6 +12,12 @@ const Page1 = () => {
                         <Link className='firstPage__lang-item' to=''>Ru</Link>
                         <Link className='firstPage__lang-item' to=''>En</Link>
                     </div>
+                    <div className='firstPage__content'>
+                        <h2 className='firstPage__content-title'>Портал Выпускников</h2>
+                        <h2 className='firstPage__content-subtitle'>
+                            Наше будущее в наших руках
+                        </h2>
+                    </div>
                 </div>
             </div>
         </section>
