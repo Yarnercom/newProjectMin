b3b41f7ad185a253cfcc048d5097abb50a61d69c
ACER <a.doroev@mail.ru>
2022-04-23 10:11:32 +0600

    fisrtCommit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react-dom'
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      awdwd
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='container'>
+                <div className='header__container'>
+
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
